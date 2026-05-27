decff633a0845b959f4e703b06dcf5d219038db9
LanaNew <l4n.steen@gmail.com>
2026-05-27 16:17:11 +0400

    ტესტ

--- lect1/Program.cs
+++ lect1/Program.cs
@@ -4,7 +4,75 @@
 	{
 		static void Main(string[] args)
 		{
+
+
+
+
 			Console.WriteLine("Hello, World!");
+			Console.ReadLine();
+
+
+
+			//cw
+
+
+			// console.log
+			// prompt 
+
+
+			//for(let i =0; i < 10; i++)
+
+			//for (let i = 0; i < 10; i++)
+
+			//var x = 10;
+			//var x 
+			//X = 100;
+
+
+
+
+			//char userInput = '+';
+			//string userName = "John";
+
+
+			//byte age = 255;    // 0 - 255    
+			//int num = 5555;
+			//uint x = 5656;
+			//long y = 5555555555;
+
+
+			//float f = 3.1415f;
+			//double pi = 3.1415;
+			//decimal dec = 3.1415m;
+
+			//bool isTrue = false;
+
+
+
+
+			Console.WriteLine("Enter your names");
+			string usernameInput = Console.ReadLine();
+
+			Console.WriteLine("your name is " + usernameInput);
+
+
+
+			string ageString = Console.ReadLine(); // "25"
+
+			byte age = byte.Parse(ageString);
+			int age5 = int.Parse(ageString);
+
+
+
+
+			
+
 		}
 	}
 }
+
+
+// gatana  -
+// shetana  +
+// balansisshemowmeba /
+// @ 

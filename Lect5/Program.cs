fd5310329feac7d0340abeb149366d76c0120a2e
LanaNew <l4n.steen@gmail.com>
2026-06-10 18:14:21 +0400

    array 2 + string

---
+++ Lect5/Program.cs
@@ -0,0 +1,211 @@
+﻿namespace Lect5
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			#region hw
+
+
+			//გააკეთე კონსოლიდან შემოყვანილი რიცხვის გამრავლების ტაბულის ერთი ბლოკი.
+			//(ათის ნამრავლის ჩათვლით)
+
+			//Console.WriteLine("Enter number");
+
+			////int num = Console.ReadLine();
+
+
+			//bool isValid = int.TryParse(Console.ReadLine(), out int num);
+
+			//if (isValid)
+			//{
+			//	for (int i = 0; i <= 10; i+=2)
+			//	{
+			//		Console.WriteLine($" {num} * {i} = {num * i}");
+			//	}
+			//}
+
+
+
+
+			//დაწერეთ პროგრამა რომელიც გამოიტანს კონსოლში ფიფქებით შედგენილ პირამიდის ფორმას.
+			//	მაგალითად ციფრი 4–ის შეყვანისას კონსოლში გამოვა შემდეგი სახის პირამიდა:
+
+
+			//Console.WriteLine("Enter number");
+			//bool isValid = int.TryParse(Console.ReadLine(), out int lines);
+
+
+			//for (int i = 1; i <= lines; i++)  //4
+			//{
+
+			//	for (int j = 1; j <= lines - i; j++)
+			//	{
+			//		Console.Write(" ");
+			//	}
+
+			//	for (int k = 1; k <= i; k++)
+			//	{
+			//		Console.Write("* ");
+			//	}
+
+
+			//	Console.WriteLine();
+			//}
+
+
+
+
+
+
+			//დაწერეთ პროგრამა რომელიც კონსოლიდან
+			//წაკითხულ რიცხვამდე დააჯამებს ყველა 
+			//	ლუწ რიცხვს და პასუხი გამოიტანეთ კონსოლში
+
+
+
+			//Console.WriteLine("Enter number");
+			//bool valid = int.TryParse(Console.ReadLine(), out int num);
+
+			//int sum = 0;
+
+			//for (int i = 0; i < num; i+=2) 
+			//{
+			//	sum += i;
+			//	//if (i % 2 == 0)
+			//	//{
+			//	//	sum += i;
+			//	//}
+			//}
+
+
+
+			//დაწერეთ პროგრამა რომელიც აირჩევს რენდომულ რიცხვს.
+			//მომხმარებელმა შემოიყვანოს კონსოლიდან რიცხვი
+			//მანამ არ გამოიცნობს არჩეულ რენდომულ რიცხვს.
+
+			//Random rand = new Random();
+
+			//int random = rand.Next(0,100);
+
+			//Console.WriteLine("Enter number");
+			//bool valid = int.TryParse(Console.ReadLine(), out int userNum);
+
+
+
+			//	while (random != userNum || !valid )
+			//	{
+			//     	Console.WriteLine($"random is {random}");
+			//		Console.WriteLine("try again");
+			//		valid = int.TryParse(Console.ReadLine(), out userNum);
+			//	}
+
+
+
+
+			#endregion
+
+
+			//byte[] numbers = [10,50,60];  // ფიქსირებული ზომის
+
+			//numbers[3] = 50;
+			//
+			//int[] number = new int[5];   // 0 0 0 0 0
+
+			//number[0] = 1;
+			//number[1] = 1;
+			//0                 Length-1
+
+
+
+
+			//Random random = new Random();
+
+			//int[] nums = new int[5];
+
+
+			//for (int i = 0; i < nums.Length; i++)
+			//{
+			//	nums[i] = random.Next(0,100);
+			//	Console.WriteLine(nums[i]);
+
+			//}
+
+
+			//string[] names = { };
+			//string[] names2 = [ ];
+			//string[] names3 = new string[2];
+			//string[] names4 = new string[2] {"John", "George" };
+
+
+
+			//შექმენით სახელების არაი და დაბეჭდეთ
+			//ყველა ის სახელი რომელიც არის "John"
+
+
+			//string[] names = ["John", "John", "George"];
+
+			//int count = 0;
+
+
+			//for (int i = 0; i < names.Length; i++)
+			//{
+			//	if (names[i] == "John")
+			//		count++;
+			//}
+
+
+			//Console.WriteLine(count);
+
+
+			//string[] names = ["John", "John", "George"];
+			//int count = 0;
+			//foreach (string item in names)
+			//{
+			//	Console.WriteLine(item);
+			//	if(item == "John")
+			//	{
+			//		count++;
+			//	}
+			//}
+
+
+
+			//string[] names = ["John", "John", "George"];
+
+			//int ind = 0;
+
+			//foreach (var item in names)
+			//{
+			//	if(item == "John")
+			//	{
+			//		Console.WriteLine(ind);
+			//	}
+			//	ind++;
+			//}
+			//
+			//
+
+			//jagged array  - მასივები მასივი 
+
+			int[][] nums = [ [20,50,60 ],  [10,20,30], [50,60,30]  ];
+
+
+			//2D array   //   მოიძიეთ ინფორმაცია
+
+
+			foreach (var item in nums)
+			{
+
+				foreach (var num in item)
+				{
+					Console.WriteLine(num);
+				}
+
+
+			}
+
+		}
+	}
+}
---
+++ Lect6/Program.cs
@@ -0,0 +1,246 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Lect6
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			//int[] arr = [10,20]; // 0
+			//arr[0] = 1;
+			//arr[1] = 2;
+			//arr[2] = 2;   // error
+
+			//arr = new int[3];
+
+
+			//index
+			//0 1 2 3 
+			//                3   2
+			//0      arr.Length - 1
+
+
+			int[] numbs = {10,20,30 };
+			int[] numbs2 = [ 10, 20, 30 ];
+
+			int[] numb3 = new int[4];    
+			int[] numb4 = new int[10];  //[0,0,0]
+
+			int[] number5 = new int[4] { 10, 20,0,0 };
+
+
+
+			//int[] points = [55, 66, 77, 88];
+
+			//int student1 = 55;
+			//int student2 = 66;
+			//int student3 = 77;
+			//int student4 = 88;
+
+			//student1++;
+			//student2++;
+
+
+			//int x = 5;
+			//int y = x;
+
+			//Console.WriteLine(x + " " +  y );  //  5    5
+			//y++;
+			//Console.WriteLine(x + " " + y);  //  5    6
+			//								 //
+			//int[] arr1 = [20, 50, 30];
+			//int[] arr2 = arr1;
+			////  [20, 50, 30]      [20, 50, 30]
+			////
+			//arr2[0] = 100;
+
+			//[100, 50, 30]    [100, 50, 30] 
+
+
+			//value  vs referance
+			//
+
+
+			//int[][] jagged = 
+			//	[
+			//	  [20,60],
+			//	  [50,60,70,80]
+			//	];  // 
+
+			//Console.WriteLine(jagged[1][2]);
+
+			//int[][] jagged =
+			//	[
+			//	  [20,60],
+			//	  [50,60,70,80]
+			//	];
+
+
+			//foreach (var line in jagged)
+			//{
+			//	int[] arr = line;
+			//	int sum = 0;
+			//	foreach (var item in arr)
+			//	{
+			//		sum += item;
+			//	}
+			//	Console.WriteLine(sum/line.Length);
+			//}
+
+
+
+			//[20, 60]
+
+
+
+
+
+			//foreach (var line in jagged)
+			//{
+			//	//[20, 60],
+			//	foreach (var element in line)
+			//	{
+			//		Console.WriteLine(element);
+			//	}
+			//}
+
+
+
+
+
+			//			დავალება 1
+			//შექმენით ერთ განზომილებიანი ორი მასივი.
+			//შეავსეთ ორივე მასივი ელემენტებით.
+			//გააერთიანე ერთ მასივში ორივე მასივის ელემენტები.
+			//დაბეჭდეთ საბოლოოდ მიღებული მასივი.
+			//მაგალითად, თუ პირველი მასივის ელემენტების: 1 2 3
+			//ხოლო მეორე მასივის ელემენტებია : 4 5 6
+			//შედეგად უნდა მიიღოთ: resultArray = [1, 2, 3, 4, 5, 6]
+
+
+
+			//int[] num1 = [20, 60, 30, 40];
+
+			//int[] num2 = [50, 60, 70, 80];
+
+
+			//int[] arr3 = new int[num1.Length + num2.Length];
+
+			//for (int i = 0; i < num1.Length; i++)
+			//{
+			//	arr3[i] = num1[i];
+			//}
+
+			//for (int i =0; i < num2.Length; i++)
+			//{
+			//	arr3[num1.Length+i] = num2[i];
+			//}
+
+
+			//foreach (var item in arr3)
+			//{
+			//	Console.WriteLine(item);
+			//}
+
+
+			//[50, 60, 70, 80, 0, 0 , 0 ,0 ]
+
+
+
+
+
+			//			დავალება 2
+			//შექმენით ინტების მასივი და შეავსეთ ელემენტებით. მაგ: 3, 5, -4, 8, 11, 1, -1, 6
+			//კონსოლიდან გადმოეცით რაღაც რიცხვი რომელსაც შეინახავთ targetSum ცვლადში.
+			//მოძებნეთ მასივში ყველა ის ორი ელემენტი რომლის ჯამიც იქნება targetSum ტოლი და ამწყვილებისგან შექმენით მასივი.
+			//დააბრუნეთ ამ ელემენტების წყვილები კონსოლში.
+			//2 | P a g e
+			//მაგალითად: array = { 3, 5, -4, 8, 11, 1, -1, 6}
+			//			targetSum = 7
+			//შედეგად უნდა მივიღოთ: resultArray = [[1, 6], [8, -1], [-4, 11]]
+
+			//int[] numbers = [3, 5, -4, 8, 11, 1, -1, 6];
+
+			//bool isvalid = int.TryParse(Console.ReadLine(), out int targetSum);  // 7
+
+			//int count = 0;
+
+
+			//for (int i = 0; i < numbers.Length; i++)
+			//{
+			//	for (int j = i + 1; j < numbers.Length; j++)
+			//	{
+			//		if (numbers[i] + numbers[j] == targetSum)
+			//		{
+			//			count++;
+			//		}
+			//	}
+			//}
+
+
+			//int[][] resultArray = new int[count][];
+			//int index = 0;
+
+			//for (int i = 0; i < numbers.Length; i++)
+			//{
+
+			//	for (int j = i + 1; j < numbers.Length; j++)
+			//	{
+
+
+			//		if (numbers[i] + numbers[j] == targetSum)
+			//		{
+			//			resultArray[index] = [numbers[i], numbers[j]];
+			//			index++;
+			//		}
+			//	}
+			//}
+
+
+
+
+
+			string text = "text";
+
+
+			text = "     tex @t2   ";
+
+
+
+			Console.WriteLine(text[0]);
+
+
+			foreach (var symbol in text)
+			{
+				Console.WriteLine(symbol);
+			}
+
+
+
+			Console.WriteLine(text.ToLower());
+			Console.WriteLine(text.ToUpper());
+			Console.WriteLine(text.Trim());
+
+			Console.WriteLine(text.Contains("@")); ///  bool
+
+			Console.WriteLine(text.EndsWith('i'));
+
+			Console.WriteLine(text.EndsWith("com"));
+
+			Console.WriteLine(text.StartsWith('t'));
+
+
+			//2D   
+
+			//int[,] matrix = new int[2, 3];
+			//matrix[0, 1] = 10;
+
+
+		}
+	}
+}
+
+
+
+
